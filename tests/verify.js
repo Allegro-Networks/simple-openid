7fc2870bd272c770263fc3e729ff8f68dfcdc1a2
Developer <ben.j.flowers@gmail.com>
2013-06-28 14:33:51 +0100

    passing request to relying party

--- tests/verify.js
+++ tests/verify.js
@@ -25,7 +25,7 @@ test('When verify Then verifyAssertation is called on openIDConnection',function
 		},
 		fakeRelyingPartyFactory = new FakeRelyingPartyFactory(mockOpenIdConnection);
 	var jimmy = new OpenIdVerification(fakeRelyingPartyFactory);
-	jimmy.verify(function(){});
+	jimmy.verify(null,function(){});
 	assert.equal(verifyAssertationCalled,true);
 });
 
@@ -35,14 +35,14 @@ test('When verify called And no errors occur Then callback method is called',fun
 			callbackCalled = true;
 		},
 		mockOpenIdConnection = {
-			verifyAssertation : function(callback){
-				callback(undefined,{});
+			verifyAssertation : function(request,callback){
+				callback(null,{});
 			}
 
 		},
 		fakeRelyingPartyFactory = new FakeRelyingPartyFactory(mockOpenIdConnection);
 	var jimmy = new OpenIdVerification(fakeRelyingPartyFactory);
-	jimmy.verify(callback);
+	jimmy.verify(null,callback);
 	assert.equal(callbackCalled,true);
 });
 
@@ -52,15 +52,29 @@ test('When verify called And errors occur Then callback method is not called',fu
 			callbackCalled = true;
 		},
 		mockOpenIdConnection = {
-			verifyAssertation : function(callback){
+			verifyAssertation : function(request,callback){
 				callback({},{});
 			}
 			
 		},
 		fakeRelyingPartyFactory = new FakeRelyingPartyFactory(mockOpenIdConnection);
 	var jimmy = new OpenIdVerification(fakeRelyingPartyFactory);
-	jimmy.verify(callback);
+	jimmy.verify(null,callback);
 	assert.equal(callbackCalled,false);
+});
+
+test('When verify called Then request is passed into the relying party',function(){
+	var relyingPartyRequest,
+		request = {},
+		mockOpenIdConnection = {
+			verifyAssertation : function(request,callback){
+				relyingPartyRequest = request;
+			}
+		},
+		fakeRelyingPartyFactory = new FakeRelyingPartyFactory(mockOpenIdConnection);
+	var openIdVerification = new OpenIdVerification(fakeRelyingPartyFactory);
+	openIdVerification.verify(request, function(){});
+	assert.equal(request,relyingPartyRequest);
 });
 
 var FakeRelyingPartyFactory = function(openIdConnection){
@@ -74,8 +88,8 @@ var FakeRelyingPartyFactory = function(openIdConnection){
 var OpenIdVerification = function(relyingPartyFactory){
 	var relyingParty = relyingPartyFactory.create();
 	
-	function verify(callback){
-		relyingParty.verifyAssertation(function(errors){
+	function verify(request,callback){
+		relyingParty.verifyAssertation(request,function(errors){
 			if(!errors){
 				callback();
 			}
